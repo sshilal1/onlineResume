6aff7fa3f518f0ac648d8a651639a68bf5e83fdd
sshilal1 <sshilal1@33b27105-aaec-4193-ab67-0ef25e346b85>
2015-12-14 20:51:13 +0000

    Fixed welcome message position by removing span and modifying the div

--- js/helper.js
+++ js/helper.js
@@ -25,7 +25,7 @@ var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var HTMLwelcomeMsg = '<div class="welcome-message">%data%</div>';
 
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
