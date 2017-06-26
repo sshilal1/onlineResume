e565888132683c2377b1bf7b05fce88db75174de
Stephen Shilale <sshilal1@binghamton.edu>
2017-06-26 10:11:42 -0400

    added html extensions for removal of .html requests

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
-app.use(express.static("public"));
+app.use(express.static("public", {
+    extensions: ['html', 'htm'],
+}));
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
