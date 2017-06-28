960fd5851850038a5966ead5a858e552640c2266
Stephen Shilale <sshilal1@binghamton.edu>
2017-06-28 09:56:27 -0400

    added wordpress module

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ app.use(express.static("public", {
     extensions: ['html', 'htm'],
 }));
 
+var wordpress = require( "wordpress" );
+var client = wordpress.createClient({
+	url: "https://stephenshilale.wordpress.com/",
+	username: "sshilal1",
+	password: "FILL THIS IN"
+});
+
+client.getPosts(function( error, posts ) {
+	console.log( "Found " + posts.length + " posts!" );
+	for (post of posts) {
+		console.log (post.title);
+	}
+});
+
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
