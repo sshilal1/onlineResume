3ebb11d826f06d233c999967fa316e2b80d616af
Stephen Shilale <sshilal1@binghamton.edu>
2017-07-19 11:48:00 -0400

    use app.get as router, need to format wordpress response and build new blog html file

--- app.js
+++ app.js
@@ -2,25 +2,50 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
+/*
 app.use(express.static("public", {
     extensions: ['html', 'htm'],
 }));
-
-/*
-var wordpress = require( "wordpress" );
-var client = wordpress.createClient({
-	url: "https://stephenshilale.wordpress.com/",
-	username: "sshilal1",
-	password: "FILL THIS IN"
-});
-
-client.getPosts(function( error, posts ) {
-	console.log( "Found " + posts.length + " posts!" );
-	for (post of posts) {
-		console.log (post.title);
-	}
-});
 */
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+	res.sendFile('index.html', { root : __dirname });
+})
+
+app.get('/projects', function (req, res) {
+	res.sendFile('public/projects.html', { root : __dirname });
+})
+
+app.get('/about', function (req, res) {
+	res.sendFile('public/about.html', { root : __dirname });
+})
+
+app.get('/blog', function (req, res) {
+	
+	res.sendFile('public/blog.html', { root : __dirname });
+	
+	var wordpress = require( "wordpress" );
+	var client = wordpress.createClient({
+		url: "https://stephenshilale.wordpress.com/",
+		username: "***",
+		password: "***"
+	});
+
+	client.getPosts(function( error, posts ) {
+		console.log( "Found " + posts.length + " posts!" );
+		for (post of posts) {
+			if (post.status == "publish") {
+				console.log (post.title);
+				/*post.
+					content, date, modified, link
+				*/
+			}
+		}
+	});
+})
+
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
