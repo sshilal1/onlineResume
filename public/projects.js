f268e72bc858649ebd232e9fbae92b021e6de452
Stephen Shilale <sshilal1@binghamton.edu>
2017-11-22 16:02:02 -0500

    added json projects, dynamically create html content

--- public/projects.js
+++ public/projects.js
@@ -1,20 +1,60 @@
 var pcontainer = document.getElementById('projects-container');
-var fragment = document.createDocumentFragment();
 
-var roweven = document.createElement('div');
-roweven.className='row even';
+for (var project of projects) {
 
-var col12 = document.createElement('div');
-col12.className='col-sm-12';
+	var fragment = document.createDocumentFragment();
+	var roweven = document.createElement('div');
+	roweven.className='row even';
 
-var h2 = document.createElement('h2');
-var title = document.createElement('a');
-title.href = "http://www.fantasydraftkit.org";
-title.textContent = 'Fantasy Football Draft Kit';
+	// Title of app
+	var col12 = document.createElement('div');
+	col12.className='col-sm-12';
+	var h2 = document.createElement('h2');
+	var title = document.createElement('a');
+	title.href = project.link;
+	title.textContent = project.name;
+	h2.appendChild(title);
+	col12.appendChild(h2);
 
-h2.appendChild(title);
-col12.appendChild(h2);
-roweven.appendChild(col12);
-fragment.appendChild(roweven);
+	// Row of Image
+	var col4a = document.createElement('div');
+	col4a.className='col-sm-4';
+	col4a.style.cssText='text-align:-webkit-center;';
+	var imgbox = document.createElement('div');
+	imgbox.className='imgbox';
+	var projimg = document.createElement('img');
+	projimg.className='project-img';
+	projimg.src=project.img;
+	imgbox.appendChild(projimg);
+	col4a.appendChild(imgbox);
 
-//pcontainer.appendChild(fragment);
+	// Description
+	var col4b = document.createElement('div');
+	col4b.className='col-sm-4';
+	var title = document.createElement('h3');
+	title.textContent = project.description.title;
+	col4b.appendChild(title);
+
+	for (var paragraph of project.description.paragraphs) {
+		var para = document.createElement('p');
+		para.textContent = paragraph;
+		col4b.appendChild(para);
+	}
+
+	// Bars
+	var col4c = document.createElement('div');
+	col4c.className='col-sm-4';
+	var barsbox = document.createElement('div');
+	barsbox.id=project.barsid;
+	barsbox.className='column-sports';
+	col4c.appendChild(barsbox);
+
+	// Append it all
+	roweven.appendChild(col12);
+	roweven.appendChild(col4a);
+	roweven.appendChild(col4b);
+	roweven.appendChild(col4c);
+	fragment.appendChild(roweven);
+}
+
+pcontainer.appendChild(fragment);
---
+++ public/projectsJson.js
@@ -0,0 +1,15 @@
+var projects = [
+	{
+		name: 'Fantasy Football Draft Kit',
+		link: 'http://www.fantasydraftkit.org',
+		img: 'images/fantasykit.jpg',
+		description: {
+			title: 'A fantasy football draft kit, designed with React and Material UI on the front-end, with Node and Cheerio on the back-end',
+			paragraphs: [
+				'This application scrapes the web using Cheerio and MySQL for fantasy football rankings and statistics.',
+				'Organize players by a filter of your choice, compare rankings, favorite your picks, and have a clear representation of stats for every season of every player, giving users an all-in-one kit for their drafts'
+			]
+		},
+		barsid: 'draft-bars'
+	},
+];
