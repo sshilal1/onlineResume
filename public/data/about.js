121a547012a7d7a2b254c088f2a0e86c9bdc2ea6
Stephen Shilale <sshilale@thomasnet.com>
2018-01-29 15:43:52 -0500

    added jobs to about data, need to figure out how to add multiple

--- public/data/about.js
+++ public/data/about.js
@@ -5,5 +5,23 @@ var about = {
     email : "me@stephenshilale.com",
     abstract : "I build full scale, interactive web applicaitons using the newest technologies on both ends of the stack, with a focus on Javascript and ES6 features",
     year : new Date().getFullYear(),
-    tagline : "Full Stack Web Developer"
+    tagline : "Full Stack Web Developer",
+    jobs : [
+        {
+            company : "Thomas",
+            location : "New York City, NY",
+            role : "Full Stack Engineer",
+            logo : "",
+            descr : "",
+            insight : ""
+        },
+        {
+            company : "Crestron Electronics",
+            location : "Rockleigh, NJ",
+            role : "Software Developer",
+            logo : "images/crestron-logo-square.png",
+            descr : "I am responsible for creating and executing test plans to adequately verify features across a range of Digital Media products. I also pioneered a new method of testing Crestron products through web requests, which is now used across the company. I have always been eager to bring my love of web development to work related issues.",
+            insight : "Being a test engineer for the past 3 years at crestron has made test-driven development my mantra, and I brought that style of coding to all of my web development projects."
+        }
+    ]
 }
