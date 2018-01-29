096b8da8e01c31e99e521a75bda8e259465b0507
Stephen Shilale <sshilale@thomasnet.com>
2018-01-29 14:08:37 -0500

    added about me data

---
+++ public/data/aboutData.js
@@ -0,0 +1,11 @@
+var about = {
+    firstname : "Stephen",
+    lastname : "Shilale",
+    location : "New York City, NY",
+    email : "me@stephenshilale.com",
+    abstract : "I build full scale, interactive web applicaitons using the newest technologies on both ends of the stack, with a focus on Javascript and ES6 features",
+    year : new Date().getFullYear(),
+    header : {
+        tagline : "Full Stack Web Developer",
+    }
+}
