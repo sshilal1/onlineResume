e52f051aec734ce8ece0e83b1d80a121500a745c
Stephen Shilale <sshilale@thomasnet.com>
2018-01-31 16:27:42 -0500

    added cover letter content

--- public/data/about.js
+++ public/data/about.js
@@ -23,5 +23,12 @@ var about = {
             descr : "I am responsible for creating and executing test plans to adequately verify features across a range of Digital Media products. I also pioneered a new method of testing Crestron products through web requests, which is now used across the company. I have always been eager to bring my love of web development to work related issues.",
             insight : "Being a test engineer for the past 3 years at crestron has made test-driven development my mantra, and I brought that style of coding to all of my web development projects."
         }
-    ]
+    ],
+    coverletter : {
+        opening : "To whom it may concern",
+        p1 : "My current job, a Verification Engineer at Crestron Electronics, has given me all of the tools for a permanent role as a web developer. Over the last three years I have been developing in PowerShell and C#, giving me a keen eye for code issues in applications and the ability to generate solutions in the most efficient manner possible. This critical mindset led me to pioneer a new method for testing our products using modern JavaScript frameworks such as Node and Bootstrap, which our team still uses daily. I have gained an affinity for JavaScript and continue my work with it at home through numerous personal projects that can be seen on my website: stephenshilale.com.",
+        p2 : "I now have over three years experience with HTML, CSS, Javascript, and many other popular frameworks associated with these tools, including React and Node. I earned a nanodegree in Front End Development, from Udacity, one of the world's leading online education organizations, that I obtained in 2016. What separates me from other candidates in my field is my strong social and communication skills. I am a collaborator and a team player with the ability to work independently when required. My eagerness to continue working with all the newest JavaScript frameworks and my desire to expand on my current knowledge of web technologies would make me a great addition to any team.",
+        exit : "Thank you for your consideration and have a wonderful day.",
+        signoff : "Best Regards"
+    }
 }
