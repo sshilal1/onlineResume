5cf011ba641395665c44627f2dd38946dcfa3b78
Stephen Shilale <sshilale@thomasnet.com>
2018-02-01 08:42:05 -0500

    added copyright footer

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,9 @@ nameC.textContent = about.firstname + " " + about.lastname;
 
 var taglineC = document.getElementById('page-title__tagline');
 taglineC.textContent = about.tagline;
+
+var footername = document.getElementById('footer-name');
+footername.innerHTML = `<span class="glyphicon glyphicon-copyright-mark"></span> ${about.firstname} ${about.lastname}`;
+
+var footerdate = document.getElementById('footer-date');
+footerdate.innerHTML = new Date().getFullYear();;
